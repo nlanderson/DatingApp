3a99313b7fd4804bdf551e9b5d15a3b21986ef6f
Neil Anderson <neil.anderson@live.com>
2019-07-16 14:26:56 -0500

    End of S3

---
+++ DatingApp.API/Controllers/AuthController.cs
@@ -0,0 +1,95 @@
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using DatingApp.API.Data;
+using DatingApp.API.Dtos;
+using DatingApp.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+namespace DatingApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthRepository _repo;
+        private readonly IConfiguration _config;
+
+        public AuthController(IAuthRepository repo, IConfiguration config)
+        {
+            _repo = repo;
+            _config = config;
+        }
+
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(UserForRegisterDto userForRegisterDto)
+        {
+            userForRegisterDto.UserName = userForRegisterDto.UserName.ToLower();
+
+            if (await _repo.UserExists(userForRegisterDto.UserName)) return BadRequest("Username already exists");
+
+            var userToCreate = new User
+            {
+                UserName = userForRegisterDto.UserName
+            };
+
+            var createdUser = await _repo.Register(userToCreate, userForRegisterDto.Password);
+
+            // TODO: change to CreatedAtRoute
+            return StatusCode(201);
+        }
+
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
+        {
+            var userFromRepo = await _repo.Login(
+                userForLoginDto.UserName.ToLower(), userForLoginDto.Password);
+
+            // make sure the passed in user not null.
+            if (userFromRepo == null) 
+                return Unauthorized();
+
+            // create the claims for the user attempting login.
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, userFromRepo.Id.ToString()),
+                new Claim(ClaimTypes.Name, userFromRepo.UserName)
+            };
+
+            // have the server sign the token with our security key.
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(
+                _config.GetSection("AppSettings:Token").Value));
+
+            // create the credentials.
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            // create the token descriptor
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(1),
+                SigningCredentials = creds
+            };
+
+            // create the JSON web token handler
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            // create the token
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            // pass the token to the user.
+            return Ok(new {
+                token = tokenHandler.WriteToken(token)
+            });
+
+        }
+
+
+    }
+}
