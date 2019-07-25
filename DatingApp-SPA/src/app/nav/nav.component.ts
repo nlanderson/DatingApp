95b2cdf52b3179cb4347c075833632ebeca1251d
Neil Anderson <neil.anderson@live.com>
2019-07-25 15:31:29 -0500

    User functionality (in progress)

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -12,7 +13,8 @@ export class NavComponent implements OnInit {
   // store user name and password
   model: any = {};
 
-  constructor(public authService: AuthService, private alertify: AlertifyService) { }
+  constructor(public authService: AuthService, private alertify: AlertifyService, 
+    private router: Router) { }
 
   ngOnInit() {
   }
@@ -22,6 +24,8 @@ export class NavComponent implements OnInit {
       this.alertify.success('Logged In Successfully!');
     }, error => {
       this.alertify.error(error);
+    }, () => {
+      this.router.navigate(['/members']);
     });
   }
 
@@ -33,6 +37,7 @@ export class NavComponent implements OnInit {
   logOut() {
     localStorage.removeItem('token');
     this.alertify.message('Logged Out Successfully.');
+    this.router.navigate(['/home']);
   }
 
 }
